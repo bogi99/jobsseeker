73d8f28b82af86c11458785648fe085490156f3f
sam <bogi99@gmail.com>
2025-12-18 08:26:25 -0500

    Added routing for about page and updated a menubar with better text

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\JobsListingController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
 
 // Jobs
@@ -14,3 +13,4 @@ Route::get('/jobs/{post}', [JobsListingController::class, 'show'])->name('jobs.s
 // Legal pages
 Route::view('/terms', 'terms')->name('terms');
 Route::view('/privacy', 'privacy')->name('privacy');
+Route::view('/about', 'about')->name('about');
