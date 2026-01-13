ea582c984a0223300014e7cbeecc3d3fb8ca5ca3
sam <bogi99@gmail.com>
2026-01-13 05:02:47 -0500

    updated route for customer panel

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,23 @@
 <?php
 
+use App\Http\Controllers\Auth\CustomerLoginController;
+use App\Http\Controllers\Auth\CustomerRegistrationController;
+
 use App\Http\Controllers\JobsListingController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
+
+Route::get('/customer/login', [CustomerLoginController::class, 'create'])
+    ->name('filament.customer.auth.login');
+Route::post('/customer/login', [CustomerLoginController::class, 'store'])
+    ->name('customer.login.store');
+
+Route::get('/customer/register', [CustomerRegistrationController::class, 'create'])
+    ->name('customer.register');
+Route::post('/customer/register', [CustomerRegistrationController::class, 'store'])
+    ->name('customer.register.store');
 
 // Jobs
 Route::get('/jobs', [JobsListingController::class, 'index'])->name('jobs.index');
